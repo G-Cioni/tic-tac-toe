8f2dc4a473152d102cdc92a34a12db6144d80637
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-08-04 18:56:25 +0200

    created detailed layout

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-const Gameboard = (() => {
-	const gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-	return { gameBoard };
-})();
-
-const player = (name) => {
-	return { name };
+const playerFactory = (name, sign) => {
+	moveCount = 0;
+	makeMove = (e) => {
+		gameBoard.splice(e, 1, sign);
+		moveCount++;
+	};
+	return { name, sign, moveCount, makeMove };
 };
 
-const Gameflow = (() => {})();
+const tileFactory = (id) => {
+	const tile = document.getElementById(id);
+	tile.addEventListener('click', () => move(id));
+	return { tile };
+};
 
-const Players = (() => {
-	const playerOne = player('Player 1');
-	const playerTwo = player('Player 2');
-
-	return { playerOne, playerTwo };
+const Gameflow = (() => {
+	p1 = playerFactory('Player 1', 'X');
+	p2 = playerFactory('Player 2', 'O');
+	move = (id) => {
+		p1.moveCount <= p2.moveCount ? p1.makeMove(id) : p2.makeMove(id);
+		checkResult();
+		declareResult();
+	};
+	checkResult = () => {};
+	declareResult = () => {};
+	return { p1, p2, move };
 })();
+
+const Gameboard = (() => {
+	const gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+	const tileArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+	tileArray.map((tile) => {
+		const i = tileArray.indexOf(tile);
+		return (tileArray[i] = tileFactory(i));
+	});
+	render = () => {};
+	return { gameBoard, tileArray, render };
+})();
+
+console.log(Gameboard);
