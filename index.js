dd604d916f9c223ba62f9f4be91e523de95ad238
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-08-05 03:37:10 +0200

    added comments

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const playerFactory = (name, sign) => {
 
 const compFactory = (name, sign) => {
 	let { moveCount } = playerFactory();
+	// unbeatable makeMove function
 	const makeMove = () => {
 		switch (true) {
 			case typeof Gameboard.gameBoard[4] !== 'string':
@@ -265,10 +266,11 @@ const compFactory = (name, sign) => {
 				randomMove();
 		}
 	};
+	//random Number generator
 	const randomInt = (max) => {
 		return Math.floor(Math.random() * max);
 	};
-
+	// randomMove for when it doesn't matter
 	const randomMove = () => {
 		let id = randomInt(9);
 		while (typeof Gameboard.gameBoard[id] === 'string') {
@@ -281,23 +283,30 @@ const compFactory = (name, sign) => {
 	return { name, sign, moveCount, makeMove };
 };
 
+//links tiles from html to javascript
 const tileFactory = (id) => {
 	const tile = document.getElementById(id);
 	tile.addEventListener('click', () => Gameflow.move(id));
 	return { tile };
 };
 
+//Gameboard module
 const Gameboard = (() => {
+	//Pop-up to display winner / tie
 	const popUp = document.getElementById('pop-up');
+	//Restart button
 	const restart = document.getElementById('restart');
 	restart.addEventListener('click', () => Gameflow.restartGame());
-
+	// create initial arrays for gameBoard and tiles
 	const gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 	const tileArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+	// Create all tiles within the tileArray
 	tileArray.map((tile) => {
 		const i = tileArray.indexOf(tile);
 		return (tileArray[i] = tileFactory(i).tile);
 	});
+	//renders html
 	const render = () => {
 		tileArray.forEach((tile) => {
 			const i = gameBoard[tileArray.indexOf(tile)];
@@ -307,36 +316,46 @@ const Gameboard = (() => {
 	return { gameBoard, tileArray, render, popUp, restart };
 })();
 
+//Gameflow Module
 const Gameflow = (() => {
+	//Create player + CPU
 	const p1 = playerFactory('You', 'X');
 	const p2 = compFactory('CPU', 'O');
+
+	//Makes a move based on who's turn it is. If it's the users turn it waits for a click event to trigger
 	const move = (id) => {
+		// this array allows me to check if all tiles have already been used. It prevents an infite while loop and allows to check for a tie
 		const array = Gameboard.gameBoard.filter((x) => typeof x === 'number');
-		console.log(array);
 		if (
 			typeof Gameboard.gameBoard[id] !== 'string' &&
 			Gameboard.popUp.textContent == ''
 		) {
+			//check if it's player 1 or player 2's turn
 			if (p1.moveCount <= p2.moveCount) {
 				p1.makeMove(id);
 				p1.moveCount++;
+				// if not all tiles have been used
 				if (array[1] !== undefined) {
-					console.log(array);
+					//This allows for automatic CPU move by rerunning move() from the start
 					setTimeout(function () {
 						move();
 					}, 150);
 				}
+				//CPU move which is run after player 1 makes his/her move
 			} else {
 				p2.makeMove();
 				p2.moveCount++;
 			}
+			// Render html
 			Gameboard.render();
+			//declare the winner if checkResult() has found one otherwise tie if all tiles have been used
 			declareResult(checkResult());
 		}
 	};
 	const checkResult = () => {
+		//array needed to check if all tiles have been used so it can declare a tie
 		const array = Gameboard.gameBoard.filter((x) => typeof x === 'number');
-		console.log(array);
+		//check for winner
 		switch (true) {
 			case Gameboard.gameBoard[0] === Gameboard.gameBoard[1] &&
 				Gameboard.gameBoard[1] === Gameboard.gameBoard[2]:
@@ -374,6 +393,7 @@ const Gameflow = (() => {
 		}
 	};
 
+	//takes checkResult() as an input and declares winner / tie
 	const declareResult = (result) => {
 		if (result === 'tie') {
 			Gameboard.popUp.textContent = "It's a tie";
@@ -387,6 +407,7 @@ const Gameflow = (() => {
 		}
 	};
 
+	// resets the game
 	const restartGame = () => {
 		location.reload();
 	};
